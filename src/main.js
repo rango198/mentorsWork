ed9bdeae4f1c18bbcde377b827b8693e0e4fc4c8
Anastasiia-Rodzina <pototzkaja.n@gmail.com>
2023-11-11 10:43:00 +0200

    add function onSub

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+import { formEl } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", onSub);
+
+function onSub (event) {
+    event.preventDefault();
+const value =  event.target.elements.message.value.trim();
+if (!value) {
+    return;
+}
+console.log(value)
+event.target.reset()
+};
