fa113c75250e4c2f5ae2d2ccdffd3bdf92caec57
BizonNT <137486029+BizonNT@users.noreply.github.com>
2023-11-11 11:32:56 +0200

    add sava data functin

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,14 @@
 const STORAGE_KEY = "tu-do-list";
+
+export function saveData(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+export function getData() {
+  try {
+    const result = localStorage.getItem(STORAGE_KEY);
+    return result ? JSON.parse(result) : [];
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 import { formEl } from "./refs";
+import { saveData, getData } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
 formEl.addEventListener("submit", onSub);
 
-function onSub (event) {
-    event.preventDefault();
-const value =  event.target.elements.message.value.trim();
-if (!value) {
+function onSub(event) {
+  event.preventDefault();
+  const value = event.target.elements.message.value.trim();
+  if (!value) {
     return;
+  }
+  console.log(value);
+  event.target.reset();
+  const array = getData();
+  const dataItem = fabric(value);
+  array.push(dataItem);
+  saveData(array);
 }
-console.log(value)
-event.target.reset()
-};
+
+function fabric(value) {
+  const id = Date.now();
+  return { id, checked: false, value };
+}
