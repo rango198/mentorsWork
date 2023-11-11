8594d2051716eaae58829d46a9581b349d2804f8
NataliaPot <137684728+NataliaPot@users.noreply.github.com>
2023-11-11 11:13:18 +0100

    addMarkup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { formEl } from "./refs";
+import { formEl, listEl } from "./refs";
+import { createMarkup } from "./markup";
 import { saveData, getData } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
@@ -17,9 +18,26 @@ function onSub(event) {
   const dataItem = fabric(value);
   array.push(dataItem);
   saveData(array);
+  const markup = createMarkup([dataItem]);
+  addMarkup(markup);
 }
 
 function fabric(value) {
   const id = Date.now();
   return { id, checked: false, value };
+}
+
+function onLoad() {
+  const items = getData();
+  if (items.length === 0) {
+    return;
+  }
+  const markup = createMarkup(items);
+  addMarkup(markup);
+}
+
+onLoad();
+
+function addMarkup(markup) {
+  listEl.insertAdjacentHTML("beforeend", markup);
 }
